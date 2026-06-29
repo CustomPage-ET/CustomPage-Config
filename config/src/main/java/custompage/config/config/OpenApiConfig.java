d1b63036d455c6cd5a13dbdf395391a493f96e38
VictoriaCast08 <victoriacastillopino88@gmail.com>
2026-06-29 00:59:22 -0400

    config + model + repository

--- config/src/main/java/custompage/config/config/OpenApiConfig.java
+++ config/src/main/java/custompage/config/config/OpenApiConfig.java
@@ -1,4 +1,18 @@
 package custompage.config.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CustomPage - Microservicio de Configuración Visual (SaaS)")
+                        .version("1.0.0")
+                        .description("API para la personalización estética, bloques y maquetación web de las PYMEs"));
+    }
 }
