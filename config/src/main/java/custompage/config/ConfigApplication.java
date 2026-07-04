1f498f4290f99a429fe27e0e749d61ef3f9aca91
VictoriaCast08 <victoriacastillopino88@gmail.com>
2026-07-03 23:45:25 -0400

    .

--- config/src/main/java/custompage/config/ConfigApplication.java
+++ config/src/main/java/custompage/config/ConfigApplication.java
@@ -2,8 +2,12 @@ package custompage.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("custompage.config.entity") // Asegura que Docker encuentre las entidades de la BD
+@EnableJpaRepositories("custompage.config.repository") // Asegura que encuentre los repositorios
 public class ConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
